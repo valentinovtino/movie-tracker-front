507c2dad2b58d3ae8437d306c2d30369fac9f2aa
Valentino Valentin <valentinovalentine95@gmail.com>
2018-05-15 16:52:28 -0600

    add intialApi call and test suite

--- src/apiCalls/api.js
+++ src/apiCalls/api.js
@@ -1,6 +1,11 @@
 export const fetchMovieData = async (url) => {
-  const response = await fetch(url)
-  const data = await response.json()
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+
 };
