e89c3356aa9e570ede826f321fd1ac8ba9da9f43
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-21 23:51:59 -0600

    Add favorites display
    Took a lot of tweaking to realize I was making a dumb dumb dumb dumb mistake. Coding this late is not good! Cleanup will include refactoring where available, adding significant CSS changes to indicate which panel of the website is active, and somehow probably purging my server of mock logins

--- src/Components/FavoritesDisplay/FavoritesDisplay.js
+++ src/Components/FavoritesDisplay/FavoritesDisplay.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import Card from '../Card/Card';
+import FavoriteCard from '../FavoriteCard/FavoriteCard';
 import { cleanData } from '../../apiCalls/helper';
 
 export const FavoritesDisplay = (props) => {
   let cards;
   const determineDisplay = () => {
     if (props.favorites) {
-      const parsedMovies = cleanData(props.favorites);
-      cards = parsedMovies.map((movie) => {
+      cards = props.favorites.map((movie) => {
         return (
-          <Card movie={movie} key={movie.id} addUserFavorite={props.addUserFavorite} userID={props.userID} />
+          <FavoriteCard 
+            movie={movie} 
+            key={movie.id} 
+            addUserFavorite={props.addUserFavorite} 
+            userID={props.user_ID}
+          />
         );
       });
-      return this.props.favorites.length > 0 ?
-        { cards } : <h3> You haven't added any favorites yet! </h3>;
+      console.log(cards)
+      return props.favorites.length > 0 ?
+        cards : <h3> You haven't added any favorites yet! </h3>;
     } else {
       return <h3> You should log in to track your favorites </h3>;
     }
-  }
+  };
 
-  
+  cards = determineDisplay()
 
   return (
-    <div>
-      {determineDisplay()}
+    <div className='Card-container'>
+      {cards}
     </div>
   );
 };
