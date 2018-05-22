aa32174da85a4ca470251e96c3406c82cb1c42fa
Helen Dechat <hdechat@gmail.com>
2018-05-22 10:30:15 -0600

    Add FavoritesDisplay tests

---
+++ src/Components/CardHolder/CardHolder.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CardHolder from './CardHolder';
+import ReactDOM from 'react-dom'
+
+describe('CardHolder', () => {
+  let wrapper;
+  let mockAddUserFavorite = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<CardHolder addUserFavorite={mockAddUserFavorite} movies={[]} userID={null} />)
+  });
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+});
---
+++ src/Components/FavoritesDisplay/FavoritesDisplay.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { FavoritesDisplay } from './FavoritesDisplay';
+import ReactDOM from 'react-dom'
+
+describe('FAVORITES DISPLAY', () => {
+  let wrapper;
+  let mockFaves = [{movie: 'Move'}]
+
+  beforeEach(() => {
+    wrapper = shallow(<FavoritesDisplay favorites={mockFaves} user_ID={null} />);
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render cards if favorites array has objects', () => {
+    expect(wrapper.find('FavoriteCard').length).toEqual(1)
+  });
+
+  it('should render h3 message element if there are no favorites', () => {
+    wrapper = shallow(<FavoritesDisplay favorites={[]} user_ID={null} />);
+
+    expect(wrapper.find('FavoriteCard').length).toEqual(0);
+    expect(wrapper.find('h3').length).toEqual(1);
+  });
+});
