096b904bad37627af8249de0db2569ce14fcc934
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-21 22:56:22 -0600

    Add favoritesDisplay component

---
+++ src/Components/FavoritesDisplay/FavoritesDisplay.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const FavoritesDisplay = (props) => {
+  const parsedMovies = cleanData(props.favorites);
+  const cards = parsedMovies.map((movie) => {
+    return (
+      <Card movie={movie} key={movie.id} addUserFavorite={props.addUserFavorite} userID={props.userID} />
+    );
+  });
+
+  const display = this.props.favorites.length > 0 ? 
+    {cards} : <h3> You haven't added any favorites yet! </h3>;
+
+  return (
+    <div>
+      {display}
+    </div>
+  );
+};
