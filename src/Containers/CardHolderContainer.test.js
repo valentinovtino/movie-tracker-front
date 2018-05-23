ff20bb04281bc9eed2f4fa39be133d30e2eb58d3
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-22 22:22:18 -0600

    Add LogInPageContainer tests

--- src/Containers/CardHolderContainer.test.js
+++ src/Containers/CardHolderContainer.test.js
@@ -3,24 +3,24 @@ import { mockObj } from '../mockData';
 import { addFavorite} from '../Actions/Actions'
 
 describe('CardHolderContainer', () => {
-    it('should return an array of movies and the user id', () => {
-      const mockState = {
-        movies: mockObj.results,
-        user: {id: 15}
-      };
-      const expected = {movies: mockObj.results, userID: 15}
+  it('should return an array of movies and the user id', () => {
+    const mockState = {
+      movies: mockObj.results,
+      user: {id: 15}
+    };
+    const expected = {movies: mockObj.results, userID: 15}
 
-      const mappedProps = mapStateToProps(mockState)
+    const mappedProps = mapStateToProps(mockState)
 
-      expect(mappedProps).toEqual(expected)
-    });
+    expect(mappedProps).toEqual(expected)
+  });
 
-    it('should return dispatch of the addFavorite action', () => {
-      const dispatch = jest.fn();
-      const actionToDispatch = addFavorite()
+  it('should return dispatch of the addFavorite action', () => {
+    const dispatch = jest.fn();
+    const actionToDispatch = addFavorite()
 
-      const mappedDispatch = mapDispatchToProps(dispatch)
+    const mappedDispatch = mapDispatchToProps(dispatch)
 
-      expect(mappedDispatch).toHaveBeenCalledWith(actionToDispatch);
-    });
+    expect(mappedDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
 });
