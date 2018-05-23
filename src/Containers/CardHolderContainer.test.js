5aceefce808a70b6ffc2b8f45fa191f82a3780f1
Helen Dechat <hdechat@gmail.com>
2018-05-22 21:33:42 -0600

    Add some container tests

--- src/Containers/CardHolderContainer.test.js
+++ src/Containers/CardHolderContainer.test.js
@@ -1,16 +1,26 @@
-import { mapStateToProps } from './CardHolderContainer';
+import { mapStateToProps, mapDispatchToProps } from './CardHolderContainer';
 import { mockObj } from '../mockData';
+import { addFavorite} from '../Actions/Actions'
 
 describe('CardHolderContainer', () => {
-    it('should return an array of movies', () => {
+    it('should return an array of movies and the user id', () => {
       const mockState = {
-        movies: mockObj.results
+        movies: mockObj.results,
+        user: {id: 15}
       };
+      const expected = {movies: mockObj.results, userID: 15}
 
-      let expected = mockObj.results 
-
-      const mappedProps = mapStateToProps(mockState).movies
+      const mappedProps = mapStateToProps(mockState)
 
       expect(mappedProps).toEqual(expected)
+    });
+
+    it('should return dispatch of the addFavorite action', () => {
+      const dispatch = jest.fn();
+      const actionToDispatch = addFavorite()
+
+      const mappedDispatch = mapDispatchToProps(dispatch)
+
+      expect(mappedDispatch).toHaveBeenCalledWith(actionToDispatch);
     });
 });
